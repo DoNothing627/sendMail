2d9e44c27d136b5c3d254c24a905377851be0d51
DoNothing627 <62040234+DoNothing627@users.noreply.github.com>
2020-12-16 12:04:21 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sendMail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
